8b12ab8b9b711238bcaaa3d860ee208502265cc3
Will Powell <me@willpowell.uk>
2024-06-29 10:58:49 +0100

    improved analysis

--- src/ml_pipeline/analysis/model_results_analysis.py
+++ src/ml_pipeline/analysis/model_results_analysis.py
@@ -1,4 +1,4 @@
-import json
+import os
 import numpy as np
 import seaborn as sns
 import pandas as pd
@@ -28,7 +28,8 @@ class ModelResultsAnalysis:
     def print_metrics(self, metrics):
         for model, metric in metrics.items():
             print(f"{model}:")
-            print(f"  Subject ID: {metric['subject_id']}")
+            if 'subject_id' in metric:
+                print(f"  Subject ID: {metric['subject_id']}")
             print(f"  Accuracy: {metric['accuracy']:.5f}")
             print(f"  Precision: {metric['precision']:.5f}")
             print(f"  Recall: {metric['recall']:.5f}")
@@ -45,75 +46,6 @@ class ModelResultsAnalysis:
         for model_name, metrics in subject_results.items():
             num_of_labels = len(metrics['confusion_matrix'])
             self.plot_confusion_matrix(num_of_labels, cm=metrics['confusion_matrix'])
-
-    def analyze_collective(self):
-        collective_metrics = {}
-        all_results = []
-
-        for model_name in self.results[0].keys():
-            accuracies = []
-            precisions = []
-            recalls = []
-            f1_scores = []
-            inference_times = []
-            
-            for subject_results in self.results:
-                subject_id = subject_results['subject_id']
-                accuracies.append(subject_results[model_name]['accuracy'])
-                precisions.append(subject_results[model_name]['precision'])
-                recalls.append(subject_results[model_name]['recall'])
-                f1_scores.append(subject_results[model_name]['f1_score'])
-                inference_times.append(subject_results[model_name]['inference_time_ms'])
-
-                all_results.append({
-                    'subject_id': subject_id,
-                    'model_name': model_name,
-                    'accuracy': subject_results[model_name]['accuracy'],
-                    'precision': subject_results[model_name]['precision'],
-                    'recall': subject_results[model_name]['recall'],
-                    'f1_score': subject_results[model_name]['f1_score'],
-                    'inference_time_ms': subject_results[model_name]['inference_time_ms']
-                })
-            
-            avg_accuracy = np.mean(accuracies)
-            avg_precision = np.mean(precisions)
-            avg_recall = np.mean(recalls)
-            avg_f1_score = np.mean(f1_scores)
-            avg_inference_time = np.mean(inference_times)
-            
-            collective_metrics[model_name] = {
-                'accuracy': avg_accuracy,
-                'precision': avg_precision,
-                'recall': avg_recall,
-                'f1_score': avg_f1_score,
-                'inference_time_ms': avg_inference_time
-            }
-
-        # Convert results to DataFrame
-        results_df = pd.DataFrame(all_results)
-        display(results_df)
-
-        # Convert DataFrame to LaTeX and print
-        latex_table = results_df.to_latex(index=False)
-        print(latex_table)
-
-        self.print_metrics(collective_metrics)
-
-        for model_name, metrics in collective_metrics.items():
-            all_cm = np.sum([subject_results[model_name]['confusion_matrix'] for subject_results in self.results], axis=0)
-            num_of_labels = len(all_cm)
-            self.plot_confusion_matrix(num_of_labels, cm=all_cm)
-
-        # Plot bar chart for each model's validation accuracy for each subject ID
-        for model_name in results_df['model_name'].unique():
-            model_data = results_df[results_df['model_name'] == model_name]
-            plt.figure(figsize=(10, 6))
-            plt.bar(model_data['subject_id'], model_data['accuracy'])
-            plt.title(f'Validation Accuracy for {model_name}')
-            plt.xlabel('Subject ID')
-            plt.ylabel('Validation Accuracy')
-            plt.ylim(0, 1)
-            plt.show()
 
     def plot_confusion_matrix(self, num_labels, cm):
         plt.figure(figsize=(10, 7))
@@ -137,3 +69,93 @@ class ModelResultsAnalysis:
             plt.yticks(fontsize=14)
 
         plt.show()
+
+    def analyze_collective(self, save_dir):
+        if not os.path.exists(save_dir):
+            os.makedirs(save_dir)
+
+        collective_metrics = {}
+        all_results = []
+
+        for model_name in self.results[0].keys():
+            accuracies = []
+            precisions = []
+            recalls = []
+            f1_scores = []
+            inference_times = []
+            model_results = []
+
+            for subject_results in self.results:
+                subject_id = subject_results[model_name]['subject_id']
+                accuracies.append(subject_results[model_name]['accuracy'])
+                precisions.append(subject_results[model_name]['precision'])
+                recalls.append(subject_results[model_name]['recall'])
+                f1_scores.append(subject_results[model_name]['f1_score'])
+                inference_times.append(subject_results[model_name]['inference_time_ms'])
+
+                result_entry = {
+                    'subject_id': subject_id,
+                    'model_name': model_name,
+                    'accuracy': subject_results[model_name]['accuracy'],
+                    'precision': subject_results[model_name]['precision'],
+                    'recall': subject_results[model_name]['recall'],
+                    'f1_score': subject_results[model_name]['f1_score'],
+                    'inference_time_ms': subject_results[model_name]['inference_time_ms']
+                }
+
+                all_results.append(result_entry)
+                model_results.append(result_entry)
+
+            avg_accuracy = np.mean(accuracies)
+            avg_precision = np.mean(precisions)
+            avg_recall = np.mean(recalls)
+            avg_f1_score = np.mean(f1_scores)
+            avg_inference_time = np.mean(inference_times)
+
+            collective_metrics[model_name] = {
+                'accuracy': avg_accuracy,
+                'precision': avg_precision,
+                'recall': avg_recall,
+                'f1_score': avg_f1_score,
+                'inference_time_ms': avg_inference_time
+            }
+
+            # Convert model-specific results to DataFrame
+            model_results_df = pd.DataFrame(model_results)
+            display(model_results_df)
+
+            # Convert DataFrame to LaTeX and save to file
+            latex_table = model_results_df.to_latex(index=False)
+            latex_file_path = os.path.join(save_dir, f'{model_name}_results_table.tex')
+            with open(latex_file_path, 'w') as f:
+                f.write(latex_table)
+            print(f'LaTeX Table for {model_name} saved to {latex_file_path}')
+
+        self.print_metrics(collective_metrics)
+
+        for model_name, metrics in collective_metrics.items():
+            all_cm = np.sum([subject_results[model_name]['confusion_matrix'] for subject_results in self.results], axis=0)
+            num_of_labels = len(all_cm)
+            fig, ax = plt.subplots()
+            self.plot_confusion_matrix(num_of_labels, cm=all_cm, ax=ax)
+            confusion_matrix_path = os.path.join(save_dir, f'{model_name}_confusion_matrix.png')
+            fig.savefig(confusion_matrix_path)
+            plt.close(fig)
+            print(f'Confusion matrix for {model_name} saved to {confusion_matrix_path}')
+
+        # Plot bar chart for each model's validation accuracy for each subject ID
+        for model_name in pd.DataFrame(all_results)['model_name'].unique():
+            model_data = pd.DataFrame(all_results)[pd.DataFrame(all_results)['model_name'] == model_name]
+            plt.figure(figsize=(10, 6))
+            bar1 = plt.bar(model_data['subject_id'], model_data['accuracy'], color='darkslategray')
+            for i, bar in enumerate(bar1):
+                            yval = bar.get_height()
+                            plt.text(bar.get_x() + bar.get_width()/2.0, yval, round(yval, 2), va='top', ha='center', fontsize=24)
+            # plt.title(f'Validation Accuracy for {model_name}')
+            plt.xlabel('Subject ID')
+            plt.ylabel('Validation Accuracy')
+            plt.ylim(0, 1)
+            bar_chart_path = os.path.join(save_dir, f'{model_name}_validation_accuracy.png')
+            plt.savefig(bar_chart_path)
+            plt.close()
+            print(f'Validation accuracy bar chart for {model_name} saved to {bar_chart_path}')
