b9df345c5f5628e1aeff283f9e84f0ab14e0a268
Will Powell <me@willpowell.uk>
2024-07-01 17:30:20 +0100

    minor changes 2

--- losocv.py
+++ losocv.py
@@ -1,14 +1,11 @@
 from src.ml_pipeline.data_loader import LOSOCVSensorDataLoader
 
-# WINDOW_LENGTH = 10
-# SLIDING_LENGTH = 2 # this will create 5 segments per 10 seconds since 10/2 = 5 with 4:1 ratio of synthetic to real samples
-# SPLIT_LENGTH = WINDOW_LENGTH # this will not sub-split the data
+WINDOW_LENGTH = 5
+SLIDING_LENGTH = WINDOW_LENGTH # this will create no overlap between segments i.e. no augmented / synthetic data.
+SPLIT_LENGTH = WINDOW_LENGTH # this will not sub-split the data
 
-WINDOW_LENGTH = 30
-SPLIT_LENGTH = int(WINDOW_LENGTH / 6) # this will sub-split the data 6 times each of 5 seconds
-SLIDING_LENGTH = SPLIT_LENGTH # this will create 6 samples per 30 seconds since 30/5 = 6 with 5:1 ratio of synthetic to real samples
-
-WRIST_CONFIG = 'config_files/dataset/wesad_wrist_configuration.json'
+# WRIST_CONFIG = 'config_files/dataset/wesad_wrist_configuration.json'
+WRIST_CONFIG = 'config_files/dataset/wesad_wrist_bvp_eda_temp_configuration.json'
 WRIST_FE = f'src/wesad/WESAD/manual_fe/wrist_manual_fe/{WINDOW_LENGTH}s_{SLIDING_LENGTH}s_{SPLIT_LENGTH}s/wrist_features.hdf5'
 dataloader_params = {
     'batch_size': 32,
@@ -18,4 +15,27 @@ dataloader_params = {
 losocv_loader = LOSOCVSensorDataLoader(WRIST_FE, WRIST_CONFIG, **dataloader_params)
 
 # Prepare the datasets
-DATASETS_PATH = losocv_loader.prepare_datasets(f'src/wesad/WESAD/datasets/wrist/all/{WINDOW_LENGTH}s_{SLIDING_LENGTH}s_{SPLIT_LENGTH}s')
+DATASETS_PATH = losocv_loader.prepare_datasets(f'src/wesad/WESAD/datasets/wrist/bvp_eda_temp/{WINDOW_LENGTH}s_{SLIDING_LENGTH}s_{SPLIT_LENGTH}s')
+
+
+
+
+
+
+WINDOW_LENGTH = 30
+SPLIT_LENGTH = int(WINDOW_LENGTH / 6) # this will sub-split the data 6 times each of 5 seconds
+SLIDING_LENGTH = SPLIT_LENGTH # this will create 6 samples per 30 seconds since 30/5 = 6 with 5:1 ratio of synthetic to real samples
+
+# WRIST_CONFIG = 'config_files/dataset/wesad_wrist_configuration.json'
+WRIST_CONFIG = 'config_files/dataset/wesad_wrist_bvp_eda_temp_configuration.json'
+WRIST_FE = f'src/wesad/WESAD/manual_fe/wrist_manual_fe/{WINDOW_LENGTH}s_{SLIDING_LENGTH}s_{SPLIT_LENGTH}s/wrist_features.hdf5'
+dataloader_params = {
+    'batch_size': 32,
+    'shuffle': True,
+    # 'num_workers': 4
+}
+losocv_loader = LOSOCVSensorDataLoader(WRIST_FE, WRIST_CONFIG, **dataloader_params)
+
+# Prepare the datasets
+DATASETS_PATH = losocv_loader.prepare_datasets(f'src/wesad/WESAD/datasets/wrist/bvp_eda_temp/{WINDOW_LENGTH}s_{SLIDING_LENGTH}s_{SPLIT_LENGTH}s')
+
