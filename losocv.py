5c4448b49f747c2d85e15a843a668ee69430f177
Will Powell <me@willpowell.uk>
2024-07-02 14:47:40 +0100

    made changes to losocv

--- losocv.py
+++ losocv.py
@@ -1,11 +1,13 @@
 from src.ml_pipeline.data_loader import LOSOCVSensorDataLoader
 
+n_folds = 5
+
 WINDOW_LENGTH = 5
 SLIDING_LENGTH = WINDOW_LENGTH # this will create no overlap between segments i.e. no augmented / synthetic data.
 SPLIT_LENGTH = WINDOW_LENGTH # this will not sub-split the data
 
 # WRIST_CONFIG = 'config_files/dataset/wesad_wrist_configuration.json'
-WRIST_CONFIG = 'config_files/dataset/wesad_wrist_bvp_eda_temp_configuration.json'
+WRIST_CONFIG = 'config_files/dataset/wesad_wrist_bvp_eda_configuration.json'
 WRIST_FE = f'src/wesad/WESAD/manual_fe/wrist_manual_fe/{WINDOW_LENGTH}s_{SLIDING_LENGTH}s_{SPLIT_LENGTH}s/wrist_features.hdf5'
 dataloader_params = {
     'batch_size': 32,
@@ -15,11 +17,7 @@ dataloader_params = {
 losocv_loader = LOSOCVSensorDataLoader(WRIST_FE, WRIST_CONFIG, **dataloader_params)
 
 # Prepare the datasets
-DATASETS_PATH = losocv_loader.prepare_datasets(f'src/wesad/WESAD/datasets/wrist/bvp_eda_temp/{WINDOW_LENGTH}s_{SLIDING_LENGTH}s_{SPLIT_LENGTH}s')
-
-
-
-
+DATASETS_PATH = losocv_loader.prepare_datasets(f'src/wesad/WESAD/datasets/wrist/bvp_eda/{WINDOW_LENGTH}s_{SLIDING_LENGTH}s_{SPLIT_LENGTH}s', n_folds=n_folds)
 
 
 WINDOW_LENGTH = 30
@@ -29,7 +27,7 @@ SPLIT_LENGTH = int(
 SLIDING_LENGTH = SPLIT_LENGTH  # this will create 6 samples per 30 seconds since 30/5 = 6 with 5:1 ratio of synthetic to real samples
 
 # WRIST_CONFIG = 'config_files/dataset/wesad_wrist_configuration.json'
-WRIST_CONFIG = 'config_files/dataset/wesad_wrist_bvp_eda_temp_configuration.json'
+WRIST_CONFIG = 'config_files/dataset/wesad_wrist_bvp_eda_configuration.json'
 WRIST_FE = f'src/wesad/WESAD/manual_fe/wrist_manual_fe/{WINDOW_LENGTH}s_{SLIDING_LENGTH}s_{SPLIT_LENGTH}s/wrist_features.hdf5'
 dataloader_params = {
     'batch_size': 32,
@@ -39,5 +37,5 @@ dataloader_params = {
 losocv_loader = LOSOCVSensorDataLoader(WRIST_FE, WRIST_CONFIG, **dataloader_params)
 
 # Prepare the datasets
-DATASETS_PATH = losocv_loader.prepare_datasets(f'src/wesad/WESAD/datasets/wrist/bvp_eda_temp/{WINDOW_LENGTH}s_{SLIDING_LENGTH}s_{SPLIT_LENGTH}s')
+DATASETS_PATH = losocv_loader.prepare_datasets(f'src/wesad/WESAD/datasets/wrist/bvp_eda/{WINDOW_LENGTH}s_{SLIDING_LENGTH}s_{SPLIT_LENGTH}s', n_folds=n_folds)
 
