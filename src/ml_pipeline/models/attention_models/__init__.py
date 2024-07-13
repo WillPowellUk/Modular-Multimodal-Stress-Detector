23118a58ab607b637de7f50fc36b5a35398c134a
Will Powell <me@willpowell.uk>
2024-07-13 19:34:07 +0100

    added moscan two

--- src/ml_pipeline/models/attention_models/__init__.py
+++ src/ml_pipeline/models/attention_models/__init__.py
@@ -1,5 +1,5 @@
 from .san_models import ModularModalityFusionNet, PersonalizedModalityFusionNet
-from .co_attention_models import ModularBCSA, MOSCAN
+from .co_attention_models import ModularBCSA, MOSCAN, MOSCANTwo
 from .attention_mechansims import (
     PositionalEncoding,
     PositionwiseFeedForward,
