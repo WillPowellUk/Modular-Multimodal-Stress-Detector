6fd27372bab1a623e58f76f36944d22e69fa1dac
Will Powell <me@willpowell.uk>
2024-07-17 11:11:49 +0100

    added accel

--- wesad_fe_chest.py
+++ wesad_fe_chest.py
@@ -1,13 +1,13 @@
 from src.ml_pipeline.data_loader import DataAugmenter
 from src.ml_pipeline.feature_extraction import ManualFE
 
-# WINDOW_LENGTH = 5
-# SLIDING_LENGTH = WINDOW_LENGTH  # this will create no overlap between segments i.e. no augmented / synthetic data.
-# SPLIT_LENGTH = WINDOW_LENGTH  # this will not sub-split the data
+WINDOW_LENGTH = 5
+SLIDING_LENGTH = WINDOW_LENGTH  # this will create no overlap between segments i.e. no augmented / synthetic data.
+SPLIT_LENGTH = WINDOW_LENGTH  # this will not sub-split the data
 
-WINDOW_LENGTH = 30
-SPLIT_LENGTH = int(WINDOW_LENGTH / 6) # this will sub-split the data 6 times each of 5 seconds
-SLIDING_LENGTH = SPLIT_LENGTH # this will create 6 samples per 30 seconds since 30/5 = 6 with 5:1 ratio of synthetic to real samples
+# WINDOW_LENGTH = 30
+# SPLIT_LENGTH = int(WINDOW_LENGTH / 6) # this will sub-split the data 6 times each of 5 seconds
+# SLIDING_LENGTH = SPLIT_LENGTH # this will create 6 samples per 30 seconds since 30/5 = 6 with 5:1 ratio of synthetic to real samples
 
 CHEST_CONFIG = "config_files/dataset/wesad_chest_all_configuration.json"
 
