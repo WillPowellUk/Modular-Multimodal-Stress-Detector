a65d2229aa98ed80efab38612fdfa885e814e7c1
Will Powell <me@willpowell.uk>
2024-07-22 11:08:53 +0100

    minor changes

--- ubfc_losocv.py
+++ ubfc_losocv.py
@@ -10,7 +10,7 @@ SLIDING_LENGTH = WINDOW_LENGTH  # this will create no overlap between segments i
 SPLIT_LENGTH = WINDOW_LENGTH  # this will not sub-split the data
 
 CONFIG = f"config_files/dataset/ubfc_{SENSORS}_configuration.json"
-FE = f"src/ubfc_phys/UBFC-PHYS/manual_fe/{WINDOW_LENGTH}s_{SLIDING_LENGTH}s_{SPLIT_LENGTH}s/all_features.hdf5"
+FE = f"src/ubfc_phys/UBFC-PHYS/manual_fe/{WINDOW_LENGTH}s_{SLIDING_LENGTH}s_{SPLIT_LENGTH}s/all_features_new.hdf5"
 
 dataloader_params = {
     "batch_size": 32,
@@ -21,7 +21,7 @@ losocv_loader = LOSOCVSensorDataLoader(FE, CONFIG, **dataloader_params)
 
 # Prepare the datasets
 DATASETS_PATH = losocv_loader.prepare_datasets(
-    f"src/ubfc_phys/UBFC-PHYS/datasets/manual_fe/{SENSORS}/{WINDOW_LENGTH}s_{SLIDING_LENGTH}s_{SPLIT_LENGTH}s", n_folds=7
+    f"src/ubfc_phys/UBFC-PHYS/datasets/manual_fe/slow/{SENSORS}/{WINDOW_LENGTH}s_{SLIDING_LENGTH}s_{SPLIT_LENGTH}s", n_folds=7
 )
 
 ############################################################################################################
@@ -34,7 +34,7 @@ SPLIT_LENGTH = int(
 SLIDING_LENGTH = SPLIT_LENGTH  # this will create 6 samples per 30 seconds since 30/5 = 6 with 5:1 ratio of synthetic to real samples
 
 CONFIG = f"config_files/dataset/ubfc_{SENSORS}_configuration.json"
-FE = f"src/ubfc_phys/UBFC-PHYS/manual_fe/{WINDOW_LENGTH}s_{SLIDING_LENGTH}s_{SPLIT_LENGTH}s/all_features.hdf5"
+FE = f"src/ubfc_phys/UBFC-PHYS/manual_fe/{WINDOW_LENGTH}s_{SLIDING_LENGTH}s_{SPLIT_LENGTH}s/all_features_new.hdf5"
 
 dataloader_params = {
     "batch_size": 32,
@@ -45,7 +45,7 @@ losocv_loader = LOSOCVSensorDataLoader(FE, CONFIG, **dataloader_params)
 
 # Prepare the datasets
 DATASETS_PATH = losocv_loader.prepare_datasets(
-    f"src/ubfc_phys/UBFC-PHYS/datasets/manual_fe/{SENSORS}/{WINDOW_LENGTH}s_{SLIDING_LENGTH}s_{SPLIT_LENGTH}s", n_folds=7
+    f"src/ubfc_phys/UBFC-PHYS/datasets/manual_fe/slow/{SENSORS}/{WINDOW_LENGTH}s_{SLIDING_LENGTH}s_{SPLIT_LENGTH}s", n_folds=7
 )
 
 '''
@@ -57,7 +57,7 @@ SLIDING_LENGTH = WINDOW_LENGTH  # this will create no overlap between segments i
 SPLIT_LENGTH = WINDOW_LENGTH  # this will not sub-split the data
 
 CONFIG = f"config_files/dataset/ubfc_{SENSORS}_configuration.json"
-FE = f"src/ubfc_phys/UBFC-PHYS/manual_fe/{WINDOW_LENGTH}s_{SLIDING_LENGTH}s_{SPLIT_LENGTH}s/all_features.hdf5"
+FE = f"src/ubfc_phys/UBFC-PHYS/manual_fe/{WINDOW_LENGTH}s_{SLIDING_LENGTH}s_{SPLIT_LENGTH}s/all_features_new.hdf5"
 
 dataloader_params = {
     "batch_size": 32,
@@ -68,7 +68,7 @@ losocv_loader = LOSOCVSensorDataLoader(FE, CONFIG, **dataloader_params)
 
 # Prepare the datasets
 DATASETS_PATH = losocv_loader.prepare_datasets(
-    f"src/ubfc_phys/UBFC-PHYS/datasets/manual_fe/{SENSORS}/{WINDOW_LENGTH}s_{SLIDING_LENGTH}s_{SPLIT_LENGTH}s", n_folds=7
+    f"src/ubfc_phys/UBFC-PHYS/datasets/manual_fe/slow/{SENSORS}/{WINDOW_LENGTH}s_{SLIDING_LENGTH}s_{SPLIT_LENGTH}s", n_folds=7
 )
 
 ############################################################################################################
@@ -81,7 +81,7 @@ SPLIT_LENGTH = int(
 SLIDING_LENGTH = SPLIT_LENGTH  # this will create 6 samples per 30 seconds since 30/5 = 6 with 5:1 ratio of synthetic to real samples
 
 CONFIG = f"config_files/dataset/ubfc_{SENSORS}_configuration.json"
-FE = f"src/ubfc_phys/UBFC-PHYS/manual_fe/{WINDOW_LENGTH}s_{SLIDING_LENGTH}s_{SPLIT_LENGTH}s/all_features.hdf5"
+FE = f"src/ubfc_phys/UBFC-PHYS/manual_fe/{WINDOW_LENGTH}s_{SLIDING_LENGTH}s_{SPLIT_LENGTH}s/all_features_new.hdf5"
 
 dataloader_params = {
     "batch_size": 32,
@@ -92,7 +92,7 @@ losocv_loader = LOSOCVSensorDataLoader(FE, CONFIG, **dataloader_params)
 
 # Prepare the datasets
 DATASETS_PATH = losocv_loader.prepare_datasets(
-    f"src/ubfc_phys/UBFC-PHYS/datasets/manual_fe/{SENSORS}/{WINDOW_LENGTH}s_{SLIDING_LENGTH}s_{SPLIT_LENGTH}s", n_folds=7
+    f"src/ubfc_phys/UBFC-PHYS/datasets/manual_fe/slow/{SENSORS}/{WINDOW_LENGTH}s_{SLIDING_LENGTH}s_{SPLIT_LENGTH}s", n_folds=7
 )
 
 
@@ -105,7 +105,7 @@ SLIDING_LENGTH = WINDOW_LENGTH  # this will create no overlap between segments i
 SPLIT_LENGTH = WINDOW_LENGTH  # this will not sub-split the data
 
 CONFIG = f"config_files/dataset/ubfc_{SENSORS}_configuration.json"
-FE = f"src/ubfc_phys/UBFC-PHYS/manual_fe/{WINDOW_LENGTH}s_{SLIDING_LENGTH}s_{SPLIT_LENGTH}s/all_features.hdf5"
+FE = f"src/ubfc_phys/UBFC-PHYS/manual_fe/{WINDOW_LENGTH}s_{SLIDING_LENGTH}s_{SPLIT_LENGTH}s/all_features_new.hdf5"
 
 dataloader_params = {
     "batch_size": 32,
@@ -116,7 +116,7 @@ losocv_loader = LOSOCVSensorDataLoader(FE, CONFIG, **dataloader_params)
 
 # Prepare the datasets
 DATASETS_PATH = losocv_loader.prepare_datasets(
-    f"src/ubfc_phys/UBFC-PHYS/datasets/manual_fe/{SENSORS}/{WINDOW_LENGTH}s_{SLIDING_LENGTH}s_{SPLIT_LENGTH}s", n_folds=7
+    f"src/ubfc_phys/UBFC-PHYS/datasets/manual_fe/slow/{SENSORS}/{WINDOW_LENGTH}s_{SLIDING_LENGTH}s_{SPLIT_LENGTH}s", n_folds=7
 )
 
 ############################################################################################################
@@ -129,7 +129,7 @@ SPLIT_LENGTH = int(
 SLIDING_LENGTH = SPLIT_LENGTH  # this will create 6 samples per 30 seconds since 30/5 = 6 with 5:1 ratio of synthetic to real samples
 
 CONFIG = f"config_files/dataset/ubfc_{SENSORS}_configuration.json"
-FE = f"src/ubfc_phys/UBFC-PHYS/manual_fe/{WINDOW_LENGTH}s_{SLIDING_LENGTH}s_{SPLIT_LENGTH}s/all_features.hdf5"
+FE = f"src/ubfc_phys/UBFC-PHYS/manual_fe/{WINDOW_LENGTH}s_{SLIDING_LENGTH}s_{SPLIT_LENGTH}s/all_features_new.hdf5"
 
 dataloader_params = {
     "batch_size": 32,
@@ -140,5 +140,5 @@ losocv_loader = LOSOCVSensorDataLoader(FE, CONFIG, **dataloader_params)
 
 # Prepare the datasets
 DATASETS_PATH = losocv_loader.prepare_datasets(
-    f"src/ubfc_phys/UBFC-PHYS/datasets/manual_fe/{SENSORS}/{WINDOW_LENGTH}s_{SLIDING_LENGTH}s_{SPLIT_LENGTH}s", n_folds=7
+    f"src/ubfc_phys/UBFC-PHYS/datasets/manual_fe/slow/{SENSORS}/{WINDOW_LENGTH}s_{SLIDING_LENGTH}s_{SPLIT_LENGTH}s", n_folds=7
 )
