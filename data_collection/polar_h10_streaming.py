0e498d535b3e9458e987b142634aa461a74ca2c9
Will Powell <me@willpowell.uk>
2024-08-06 16:51:25 +0100

    ecg and rr working

--- data_collection/polar_h10_streaming.py
+++ data_collection/polar_h10_streaming.py
@@ -8,10 +8,12 @@ sys.path.append(r'submodules')
 # Import the specific function or module from bleakheart
 from bleakheart.examples import ecg_rr_acc_recorder as recorder # type: ignore
 
-async def main():
+async def main(file_name):
     # Call the main function from ecg_queue, assuming it's an async function
-    await recorder.main()
+    await recorder.main(file_name)
 
 # Use asyncio.run() to run the main coroutine
 if __name__ == "__main__":
-    asyncio.run(main())
+    subject = 2
+    file_name = f'data_collection/recordings/S{subject}'
+    asyncio.run(main(file_name))
