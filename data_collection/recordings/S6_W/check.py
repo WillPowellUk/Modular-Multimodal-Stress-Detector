ee3287fb4cc9422c173f3dba4ec73c80072d93a3
Will Powell <me@willpowell.uk>
2024-08-15 11:05:57 +0100

    removed big files

--- data_collection/recordings/S6_W/check.py
+++
@@ -1,9 +0,0 @@
-import pickle
-
-
-# read pkl file
-pkl_path = 'data_collection/recordings/S6_W/S6.pkl'
-
-with open(pkl_path, 'rb') as f:
-    data = pickle.load(f, encoding='latin1')
-    print(data.keys())
